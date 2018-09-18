962ce4c86d08ee014f619ab064e14d87651d6af8
Surendran <surendran@hasura.io>
2018-09-18 18:35:39 +0530

    basic version of todo app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import ToDoWrapper from './ToDoWrapper';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container-fluid noPadd">
+        <div className="header">
+          This is a sample todo app
+        </div>
+        <div className="col-md-9 noPadd todoMainWrapper">
+          <div className="col-md-6 addPadTop">
+            <div className="sectionHeader">
+               Public todos
+            </div>
+            <ToDoWrapper />
+          </div>
+          <div className="col-md-6 addPadTop">
+            <div className="sectionHeader">
+              Private todos
+            </div>
+            <ToDoWrapper />
+          </div>
+        </div>
+        <div className="col-md-3 removePaddRight">
+          <div className="sliderMenu">
+            <div className="sliderHeader">
+              Online users - 3
+            </div>
+            <div className="userInfo">
+              <div className="userImg">
+                <i className="far fa-user"></i>
+              </div>
+              <div className="userName">
+                Praveen
+              </div>
+            </div>
+            <div className="userInfo">
+              <div className="userImg">
+                <i className="far fa-user"></i>
+              </div>
+              <div className="userName">
+                Karthik
+              </div>
+            </div>
+            <div className="userInfo">
+              <div className="userImg">
+                <i className="far fa-user"></i>
+              </div>
+              <div className="userName">
+                Suree
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
