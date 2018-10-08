43f60ec488ea08715c7ead9c616597898d673665
Praveen D <praveen@hasura.io>
2018-10-08 12:58:43 +0530

    subscription tests

--- react-apollo-todo/cypress/helpers/dataHelpers.js
+++ react-apollo-todo/cypress/helpers/dataHelpers.js
@@ -1,7 +1,8 @@
 export const baseUrl = Cypress.config("baseUrl");
 export const getElementFromAlias = alias => `[data-test=${alias}]`;
 export const getTodoName = (i, todoName = "") =>
-  `apic_test_todo_${todoName}_${i}`;
+  `tutorial_test_todo_${todoName}_${i}`;
+export const getUserName = (i, userName = "") => `${i}_${userName}`;
 export const makeDataAPIUrl = dataApiUrl => `${dataApiUrl}/v1/query`;
 export const makeDataAPIOptions = (dataApiUrl, key, body) => ({
   method: "POST",
