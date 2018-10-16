0f6796e3f7f42fab76cc5339eaa859c7f2383234
Praveen D <praveen@hasura.io>
2018-10-16 16:11:42 +0530

    fix online users limit

--- react-apollo-todo/src/Todo/TodoQueries.js
+++ react-apollo-todo/src/Todo/TodoQueries.js
@@ -118,7 +118,7 @@ const SUBSCRIPTION_TODO_PUBLIC_LIST = gql`
 
 const SUBSCRIPTION_ONLINE_USERS = gql`
   subscription {
-    online_users(order_by: name_asc, limit: 1) {
+    online_users(order_by: name_asc) {
       name
     }
   }
